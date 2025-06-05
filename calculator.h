07c7be01e2ee15540811721a1a617eb9982c835a
Daniela Fonseca <daniela.fonsecazumbado@gmail.com>
2025-06-05 15:18:26 -0600

    initial commit

---
+++ calculator.h
@@ -0,0 +1,14 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator {
+public:
+    int Add(int a, int b) {
+        return a + b;
+    }
+    int Substract(int a, int b) {
+        return a - b;
+    }
+};
+
+#endif // CALCULATOR_H
